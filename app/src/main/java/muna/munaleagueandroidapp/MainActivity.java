81e63122754d2e7b712fecb5d39c4c53b1044c53
minhazur93 <minhazur93@gmail.com>
2017-12-26 21:18:49 -0500

    created styles for RostersActivity

--- app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
@@ -1,7 +1,9 @@
 package muna.munaleagueandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToRostersActivity(View view) {
+        Intent intent = new Intent(this, Rosters.class);
+        startActivity(intent);
     }
 }
