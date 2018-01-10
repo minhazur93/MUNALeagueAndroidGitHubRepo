2eb7616e83bdbc02083cf2f43e4ca21132b232fa
minhazur93 <minhazur93@gmail.com>
2018-01-10 12:48:40 -0500

    created blank MediaActivity

--- app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
@@ -36,4 +36,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void goToMediaActivity(View view){
+        Intent intent= new Intent(this, MediaActivity.class);
+        startActivity(intent);
+
+    }
 }
