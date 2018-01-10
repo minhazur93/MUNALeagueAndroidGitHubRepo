0b4343749a72045646c6b33cfd1529ef68508771
minhazur93 <minhazur93@gmail.com>
2018-01-10 11:34:25 -0500

    created textViewReminder

--- app/src/main/java/muna/munaleagueandroidapp/HomeActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/HomeActivity.java
@@ -2,12 +2,16 @@ package muna.munaleagueandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView textViewReminder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        textViewReminder= (TextView) findViewById(R.id.textViewReminder);
     }
 }
