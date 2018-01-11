8aa512d94b27befe562c1be7fddb5780dd5a8bf0
juned2017 <juned.ahmed@detroitlabs.com>
2018-01-11 00:34:48 -0500

    added private methods to fireup intent

--- app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
@@ -7,28 +7,28 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -42,21 +42,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -70,27 +58,42 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_team_challenges) {
-
-            Intent intent = new Intent(this, TeamChallengesActivity.class);
-            startActivity(intent);
+           startTeamChallengesActivity();
 
         } else if (id == R.id.nav_team_rosters) {
+            startRosterActivity();
 
-            Intent intent = new Intent(this, RosterActivity.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_teams) {
-            Intent intent = new Intent(this, TeamsActivity.class);
-            startActivity(intent);
+           startTeamsActivity();
 
         } else if (id == R.id.nav_chat) {
-            Intent intent = new Intent(this, ChatActivity.class);
-            startActivity(intent);
+           startChatActivity();
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void startChatActivity() {
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
+    }
+
+    private void startTeamsActivity() {
+        Intent intent = new Intent(this, TeamsActivity.class);
+        startActivity(intent);
+    }
+
+    private void startTeamChallengesActivity(){
+        Intent intent = new Intent(this, TeamChallengesActivity.class);
+        startActivity(intent);
+    }
+
+    private void startRosterActivity(){
+        Intent intent = new Intent(this, RosterActivity.class);
+        startActivity(intent);
     }
 }
