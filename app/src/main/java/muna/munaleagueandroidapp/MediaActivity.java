35ebd73a908694513f1273c588f2414135b24bb0
minhazur93 <minhazur93@gmail.com>
2018-01-11 10:03:30 -0500

    created MediaActivity

--- app/src/main/java/muna/munaleagueandroidapp/MediaActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/MediaActivity.java
@@ -1,13 +1,36 @@
 package muna.munaleagueandroidapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MediaActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
+
+        imageView= (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void uploadPhoto(View view){
+    }
+
+    public void takePicture(View view){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 000);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 000 && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
