ba20109c18a6ade2820ee4e3f37a771c9620f4be
juned2017 <juned.ahmed@detroitlabs.com>
2018-01-11 09:57:00 -0500

    made changes to layout file

--- app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
+++ app/src/main/java/muna/munaleagueandroidapp/MainActivity.java
@@ -14,16 +14,21 @@ public class MainActivity extends AppCompatActivity
 
     private ActionBarDrawerToggle toggle;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
         toggle = new ActionBarDrawerToggle(
                 this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
